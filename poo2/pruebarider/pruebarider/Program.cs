f0cfbaa18885e8ccad5a1cd4901b5ab6ed88a25b
Fernando Quevedo <ferqueve@gmail.com>
2026-06-25 17:34:57 -0300

    UT5 subida

---
+++ poo2/pruebarider/pruebarider/Program.cs
@@ -0,0 +1,10 @@
+﻿
+class Program
+ {
+     static void Main()
+     {
+         Console.Write("Dime Tu nombre: ");
+         string nombre = Console.ReadLine();
+         Console.WriteLine("Hola " + nombre);
+     }
+}
